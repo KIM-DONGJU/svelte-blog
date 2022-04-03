bd037f13b8d9eb22068f1b1a1519c45e86fbe355
김동주 <kdj0689@gmail.com>
2022-04-03 23:16:40 +0900

    for문을 통해 component 반복 실행 예제 추가

--- src/pages/Test1.svelte
+++ src/pages/Test1.svelte
@@ -2,10 +2,18 @@
   import {
     AppBanner
   } from '@/components';
+
+  const forCount = [
+    1,
+    2,
+    3,
+  ];
 </script>
 
 <div>
-  <AppBanner
-  bannerConetents={'설명을 해보자'}
-  />
+  {#each forCount as index}
+    <AppBanner
+      bannerConetents={`설명을 해보자-${index}`}
+    />
+  {/each}
 </div>
