8f1eebbf33ffd8e23b992837084fb6015a83b144
김동주 <kdj0689@gmail.com>
2022-04-05 21:42:58 +0900

    re-rendering 테스트
    1. object 에서 key에 직접 접근해서 값 변경했을 때
    	- test.a.b.c 와 같은 형태의 값을 변경해도 re-render가 잘
    	  이루어짐
    
    2. Array에서 index에 직접 접근해서 값 변경했을 때
    	- vue에서는 변경을 감지하지 못함
    	- svelte에서는 변경을 감지함
    	- 심지어 test[1][2] 와 같은 방식으로 값을 변경해도 변경 감지
    
    3. Array 안의 Object 형태
    	- 변경 감지

--- src/pages/Test1.svelte
+++ src/pages/Test1.svelte
@@ -2,12 +2,59 @@
   import {
     AppBanner
   } from '@/components';
+import { onMount } from 'svelte';
 
-  const forCount = [
+  let forCount: Array<number> = [
     1,
     2,
     3,
   ];
+
+  let testObject: {
+    a: {
+      b: {
+        c: {
+          d: number
+        }
+      }
+    }
+  } = {
+    a: {
+      b: {
+        c: {
+          d: 0,
+        }
+      }
+    }
+  }
+
+  let testArray:Array<Array<number>> = [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
+
+  let testArrayAndObject = [
+    {
+      a: {
+        b: {
+          c: 0
+        }
+      }
+    },
+    {
+      a: {
+        b: {
+          c: 0
+        }
+      }
+    },
+  ]
+
+  onMount(() => {
+    setInterval(() => {
+      forCount[1] += 1;
+      testObject.a.b.c.d += 1;
+      testArray[1][2] += 1;
+      testArrayAndObject[1].a.b.c += 1;
+    }, 1000);
+  })
 </script>
 
 <div>
@@ -16,4 +63,7 @@
       bannerConetents={`설명을 해보자-${index}`}
     />
   {/each}
+  <p>{testObject.a.b.c.d} - test Object</p>
+  <p>{testArray[1][2]} - test Array</p>
+  <p>{testArrayAndObject[1].a.b.c}</p>
 </div>
